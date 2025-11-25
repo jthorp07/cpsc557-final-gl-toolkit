eb16c015ccc536d5c7808cdd6db318f6075c3763
=jthorp07 <=>
2025-11-25 16:38:48 -0500

    Add mouse position and mouse pressed inputs

--- src/gl_tools/Core/Input.ts
+++ src/gl_tools/Core/Input.ts
@@ -4,14 +4,22 @@
  * @brief Controls and exposes user inputs
  */
 
+import { Vector2 } from "../MatrixMath/Vector.js";
+
 export class Input {
 
     private static _instance: Input;
     private _canvas: HTMLCanvasElement;
+    private _viewportRectangle: DOMRect;
+    private _viewWidth: number;
+    private _viewHeight: number;
 
     // Mouse Move Trackers
     private _dX: number = 0;
     private _dY: number = 0;
+    private _mousePosition = new Vector2();
+    // Mouse Pressed
+    private _mousePressed = false;
     // WASD Pressed
     private _wPressed = false;
     private _aPressed = false;
@@ -20,7 +28,10 @@ export class Input {
 
     constructor(canvas: HTMLCanvasElement) {
         this._canvas = canvas;
-        this.initializeMouseMove();
+        this._viewportRectangle = canvas.getBoundingClientRect();
+        this._viewWidth = this._viewportRectangle.width;
+        this._viewHeight = this._viewportRectangle.height;
+        this.initializeMouseInputs();
         this.initializeWASD();
     }
 
@@ -32,6 +43,8 @@ export class Input {
         this._instance._dY = 0;
         return movement;
     }
+    static get MousePosition() { return this._instance._mousePosition; }
+    static get MousePressed() { return this._instance._mousePressed; }
     static get WPressed() { return this._instance._wPressed; }
     static get APressed() { return this._instance._aPressed; }
     static get SPressed() { return this._instance._sPressed; }
@@ -56,18 +69,30 @@ export class Input {
         this._instance._canvas = canvas;
     }
 
-    private initializeMouseMove() {
+    private initializeMouseInputs() {
 
         this._canvas.addEventListener("click", () => {
             this._canvas.requestPointerLock();
         });
 
         document.addEventListener("mousemove", (event) => {
+
+            // Mouse Movement
             if (document.pointerLockElement === this._canvas) {
                 this._dX += event.movementX;
                 this._dY += event.movementY;
             }
+
+            // Mouse Position
+            const relativeX = event.clientX - this._viewportRectangle.left;
+            const relativeY = event.clientY - this._viewportRectangle.top;
+            this._mousePosition.x = (relativeX / this._viewWidth) * 2 - 1;
+            this._mousePosition.y = (relativeY / this._viewHeight) * 2 + 1;
         });
+
+        // Mouse Press Detection
+        document.addEventListener("mousedown", () => { this._mousePressed = true; });
+        document.addEventListener("mouseup", () => { this._mousePressed = false; });
     }
 
     private initializeWASD() {
